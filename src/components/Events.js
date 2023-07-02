064fb948c3d9154b1125d0844f01aa92f0a5a506
ukirdeomkar <ukirdeomkar4@gmail.com>
2023-07-02 17:55:57 +0530

    init

---
+++ src/components/Events.js
@@ -0,0 +1,16 @@
+import React from "react";
+import EventItem from "./EventItem";
+
+export default function Events() {
+  return (
+    <>
+    <div className="container">
+      <h1 className="container text-center">This is EVnet </h1>
+      <EventItem />
+      <EventItem />
+      <EventItem />
+      <EventItem />
+      </div>
+    </>
+  );
+}
